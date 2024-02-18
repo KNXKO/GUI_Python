dffaebba41dc6ce6674754e3109273c74447a617
KNXKO <lukaslechovic@gmail.com>
2024-02-18 14:49:02 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+# System Settings
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Our App Frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+
+#Run app
+app.mainloop()
